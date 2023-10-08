a32a55befd909a2dabf3c0ee075cebf14e9c3d08
Kenny Zimny <kenny.zimny@criticalmass.com>
2023-10-08 10:20:46 -0500

    Set up base input and if() logic. Need refractor.

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 const content = {
+  error: 'Error_1 - Command Not Found',
   phase1: {
     messageText: 'Check Core Temperature?',
     optionsText: 'YES / NO',
     transitionDuration: 2000,
     transitionTextYes: 'Checking Core Temperature ...',
-    transitionTextNo: '',
+    transitionTextNo: 'transitionTextNo',
     returnTextYes: 'Core Temperature Normal.',
-    returnTextNo: '',
+    returnTextNo: 'returnTextNo',
     userAnswer: '',
   },
 }
--- runGame.js
+++ runGame.js
@@ -28,17 +28,39 @@ async function runGame() {
   // TODO, clean up content object
   domMessage.innerText = content.phase1.messageText
   domOptions.innerText = content.phase1.optionsText
-  console.log('A user types their answer: "YES"') // Should I await the user input in an async function();?
-  content.phase1.userAnswer = 'YES'
 
-  console.log(content.phase1)
+  console.log('Awaiting user Answer') // Should I await the user input in an async function();?
+  let answer = ''
+  addEventListener('keypress', async (e) => {
+    answer += e.key
+    if (e.key === 'Enter') {
+      if (answer === 'YESEnter') {
+        content.phase1.userAnswer = 'YES'
 
-  await transition(
-    content.phase1.transitionDuration,
-    content.phase1.transitionText
-  )
+        await transition(
+          content.phase1.transitionDuration,
+          content.phase1.transitionTextYes
+        )
 
-  domReturn.innerText = content.phase1.returnText
+        domReturn.innerText = content.phase1.returnTextYes
+        answer = ''
+      } else if (answer === 'NOEnter') {
+        content.phase1.userAnswer = 'NO'
+
+        await transition(
+          content.phase1.transitionDuration,
+          content.phase1.transitionTextNo
+        )
+
+        domReturn.innerText = content.phase1.returnTextNo
+        answer = ''
+      } else {
+        await transition(1000, '...') //error wait transition()
+        domReturn.innerText = content.error
+        answer = ''
+      }
+    }
+  })
 }
 
 setTimeout(runGame, 1200) // Quality of life, easier on eyes in debug
